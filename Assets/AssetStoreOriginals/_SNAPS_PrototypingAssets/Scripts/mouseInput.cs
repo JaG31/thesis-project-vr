db67c91ace7fce77ac53695bb4b35d669c6371a5
JaG31 <James.Gillies@uoit.net>
2020-11-07 19:06:44 -0500

    Initial commit, Open and show python file text in 2d only

---
+++ Assets/AssetStoreOriginals/_SNAPS_PrototypingAssets/Scripts/mouseInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Detects clicks from the mouse and prints a message
+// depending on the click detected.
+
+public class ExampleClass : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed primary button.");
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+    }
+}
---
+++ Assets/SteamVR/Input/VRInputScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+ using UnityEngine.EventSystems;
+ using Valve.VR.Extras;
+ 
+ public class VRInputScript : MonoBehaviour
+ {
+    public bool selected;
+    private SteamVR_LaserPointer laserPointer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        selected = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        
+        if (e.target.name == this.gameObject.name && selected==false)
+        {
+            selected = true;
+            Debug.Log("pointer is inside this object" + e.target.name);
+        }        
+    }
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        
+        if (e.target.name == this.gameObject.name && selected == true)
+        {
+            selected = false;
+            Debug.Log("pointer is outside this object" + e.target.name);
+        }
+    }
+    public bool get_selected_value()
+    {
+        return selected;
+    }
+ }
