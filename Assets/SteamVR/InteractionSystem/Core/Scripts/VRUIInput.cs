efe64ce8f316b5cd512646c9104da94ffd8e7cae
JaG31 <James.Gillies@uoit.net>
2021-02-08 05:18:22 -0500

    Some laser pointer functionality with file browser

--- Assets/SteamVR/InteractionSystem/Core/Scripts/VRUIInput.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/VRUIInput.cs
@@ -1,30 +1,30 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Valve.VR.Extras;
-
-[RequireComponent(typeof(SteamVR_LaserPointer))]
 public class VRUIInput : MonoBehaviour
 {
-    private SteamVR_LaserPointer laserPointer;
+    public SteamVR_LaserPointer laserPointer;
 
     private void OnEnable()
     {
-        laserPointer = GetComponent<SteamVR_LaserPointer>();
-        laserPointer.PointerIn -= HandlePointerIn;
         laserPointer.PointerIn += HandlePointerIn;
-        laserPointer.PointerOut -= HandlePointerOut;
         laserPointer.PointerOut += HandlePointerOut;
+        laserPointer.PointerClick += HandleTriggerClicked;
     }
 
     private void HandleTriggerClicked(object sender, PointerEventArgs e)
     {
+        Debug.Log(e.target.name);
         if (EventSystem.current.currentSelectedGameObject != null)
         {
             ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler);
         }
     }
 
+
+    //ADD ALL OF THE TYPES OF OBJECTS USED TO HERE
     private void HandlePointerIn(object sender, PointerEventArgs e)
     {
         var button = e.target.GetComponent<Button>();
@@ -33,6 +33,18 @@ public class VRUIInput : MonoBehaviour
             button.Select();
             Debug.Log("HandlePointerIn", e.target.gameObject);
         }
+
+        var toggle = e.target.GetComponent<Toggle>();
+        if (toggle != null) {
+            toggle.Select();
+        }
+
+        var dropdown = e.target.GetComponent<Dropdown>();
+        if (dropdown != null) {
+            dropdown.Select();
+        }
+
+
     }
 
     private void HandlePointerOut(object sender, PointerEventArgs e)
@@ -43,6 +55,16 @@ public class VRUIInput : MonoBehaviour
         {
             EventSystem.current.SetSelectedGameObject(null);
             Debug.Log("HandlePointerOut", e.target.gameObject);
+        }
+
+        var toggle = e.target.GetComponent<Toggle>();
+        if (toggle != null) {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+
+        var dropdown = e.target.GetComponent<Dropdown>();
+        if (dropdown != null) {
+            EventSystem.current.SetSelectedGameObject(null);
         }
     }
 }
