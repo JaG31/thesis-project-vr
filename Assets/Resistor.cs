a5216203aa5f553aa1aa2b04b599811a7b6311d4
JaG31 <James.Gillies@uoit.net>
2021-03-18 03:02:49 -0400

    Resistor only scales a certain amount, still need to prevent moving connectors at edges

--- Assets/Resistor.cs
+++ Assets/Resistor.cs
@@ -6,6 +6,8 @@ public class Resistor : MonoBehaviour {
     [SerializeField] Transform connector1;
     [SerializeField] Transform connector2;
     [SerializeField] Transform bridge;
+    [SerializeField] GameObject connector1Prefab;
+    [SerializeField] GameObject connector2Prefab;
 
     void Update() {
         if (connector1 != null && connector2 != null && bridge != null) {
@@ -17,7 +19,16 @@ public class Resistor : MonoBehaviour {
             bridge.position = (connector1.position + connector2.position) / 2f;
 
             // increase the height of the bridge to span the distance between the connectors
-            bridge.localScale = new Vector3(1f, 1f, bridgeDirection.magnitude*5);
+            Vector3 largeScale = new Vector3(1,1,1);
+            //Debug.Log(Vector3.Distance(bridge.localScale, largeScale));
+            Debug.Log(bridgeDirection.magnitude*5);
+            if (bridgeDirection.magnitude*5 > 1) {
+                
+            }
+            else {
+                bridge.localScale = new Vector3(1f, 1f, bridgeDirection.magnitude*5);
+            }
+            
         } else {
             Debug.Log("Some of the required components are null");
         }
